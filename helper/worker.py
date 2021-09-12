49a5614b736bb5a1a7bec9446ee0cf9944ff07fa
ChAuLaProduction <86785480+ChAuLaProduction@users.noreply.github.com>
2021-09-12 21:45:33 +0530

    Update worker.py

--- helper/worker.py
+++ helper/worker.py
@@ -214,7 +214,7 @@ async def encod(event):
         xxx = await event.reply("`Downloading...`")
         """ For Force Subscribe Channel"""
          pp = []
-         async for x in event.client.iter_participants("put group username"):
+         async for x in event.client.iter_participants("@chaulabots"):
             pp.append(x.id)
          if (user.id) not in pp:
             return await xxx.edit(
